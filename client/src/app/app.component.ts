a250dd50054e0394baab2613025394dea244ee00
thihuongnguyen <nguyenthihuong111289@gmail.com>
2022-07-21 10:37:33 +0930

    End of section 9: implement client side, shop features

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { IPagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { IPagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title = 'SkiNet3';
 
-  products: IProduct[];
+  constructor() {}
 
-  constructor(private http: HttpClient) {}
-
-  ngOnInit(): void {
-    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response: IPagination) => {
-      this.products = response.data;  
-    console.log(response);
-    }, error => {
-      console.log(error);
-    });
-  }
+  ngOnInit(): void {}
 }
